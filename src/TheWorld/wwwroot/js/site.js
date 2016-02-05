58edd80652fb9c90f5416cea6227c4c8e59361c9
x-radeon <austin@xradeon.com>
2016-02-04 23:43:38 -0700

    Javascript

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// site.js
+(function () {
+
+    var ele = $("#username");
+    ele.text("New Name");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "background-color: #898;";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuOptions = $("ul.menu li a");
+    menuOptions.on("click", function () {
+        var me = $(this);
+        alert(me.text())
+    });
+
+})();
