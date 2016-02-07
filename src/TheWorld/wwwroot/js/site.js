bb3632c44b344a4f586e49b2e10c62a88571e58f
x-radeon <austin@xradeon.com>
2016-02-06 23:28:11 -0700

    End of MVC 6 Module

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,22 +1,34 @@
 ﻿// site.js
 (function () {
 
-    var ele = $("#username");
-    ele.text("New Name");
+    //var ele = $("#username");
+    //ele.text("New Name");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        this.style = "background-color: #898;";
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    this.style = "background-color: #898;";
+    //});
 
-    main.on("mouseleave", function () {
-        this.style = "";
-    });
+    //main.on("mouseleave", function () {
+    //    this.style = "";
+    //});
 
-    var menuOptions = $("ul.menu li a");
-    menuOptions.on("click", function () {
-        var me = $(this);
-        alert(me.text())
-    });
+    //var menuOptions = $("ul.menu li a");
+    //menuOptions.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text())
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    })
+    
 
 })();
